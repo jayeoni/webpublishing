b0c155de1aa7d2d877a5016e495c2a54c391f7e4
JY Choi <jykchoi@gmail.com>
2022-04-06 09:31:31 +0900

    수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="m-4">
+      <div className="py-4 text-3xl font-bold mb-5 mt-10">List</div>
+      <div className="p-5 border border-gray-200">
+        <div className="p-40 bg-gray-100"></div>
+        <div className="my-3 font-semibold">Card Title</div>
+        <div className="font-light">Sed vel turpis~~</div>
+        <div className="mt-5">
+          <div className="bg-gray-800 text-white text-center py-3 px-10">
+            상세정보
+          </div>
+          <div className="text-center py-3 text-gray-800 border border-gray-800 mt-2">
+            주문하기
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div className="m-4">
+      <div className="text-3xl font-bold mb-7">회원가입</div>
+      <div>
+        <TextField label="이름" placeholder="이름을 입력해주세요." />
+        <TextField label="생년월일" placeholder="생년월일을 입력해주세요." />
+        <TextField label="휴대폰 번호" placeholder="번호를 입력해주세요." />
+        <TextField label="국적" placeholder="국적을 입력해주세요." />
+      </div>
+      <div className="text-center py-3 text-gray-800 border border-gray-800 mt-2">
+        회원가입
+      </div>
+    </div>
+  );
 };
